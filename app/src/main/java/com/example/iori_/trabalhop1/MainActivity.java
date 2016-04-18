3f0264e48ffe04201f1c5d918010f71e998d1498
cristian-trucco <cristian-trucco@bol.com.br>
2016-04-17 21:13:54 -0300

    Initial commit do trabalho Android

---
+++ app/src/main/java/com/example/iori_/trabalhop1/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.iori_.trabalhop1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText login_user;
+    EditText camposenha;
+    Button   buttonEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        login_user = (EditText) findViewById(R.id.login_user);
+        camposenha = (EditText) findViewById(R.id.camposenha);
+        buttonEntrar = (Button) findViewById(R.id.buttonEntrar);
+
+
+        //Comportamento do Botão de Entrar
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(login_user.getText().length()==0|| camposenha.getText().length()==0) {
+                    Toast.makeText(getApplication(),
+                            "Os campos login e senha são obrigatórios",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplication(),
+                            "Seja Bem Vindo: "+login_user.getText().toString()+"!",
+                            Toast.LENGTH_LONG).show();
+                    login_user.setText("");
+                    camposenha.setText("");
+
+
+                }
+
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
